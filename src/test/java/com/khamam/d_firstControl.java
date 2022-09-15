9d004eb035d168f86e9e3474689a4828aaf6196f
veraDulda <99422897+veraDulda@users.noreply.github.com>
2022-09-15 16:24:36 +0300

    using BaseURI

---
+++ src/test/java/com/khamam/d_firstControl.java
@@ -0,0 +1,46 @@
+package com.khamam;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+
+public class d_firstControl {
+
+    @BeforeAll
+    public static void init(){
+        baseURI ="http://54.91.166.114";
+        port = 8000;
+        basePath = "api";
+    }
+
+     @Test
+    public void test1(){
+
+        Response response =  given().accept(ContentType.JSON)
+                            .when()
+                            .get("spartans");
+
+        Assertions.assertEquals(response.statusCode(),200);
+        Assertions.assertEquals(response.header("Content-Type"),"application/json");
+
+    }
+
+    @Test
+    public void test2(){
+
+        Response response = given().accept(ContentType.JSON)
+                         .get("spartans/131");
+
+        response.prettyPrint();
+        Assertions.assertEquals(response.statusCode(),200);
+        Assertions.assertEquals(response.header("Content-Type"),"application/json");
+
+
+    }
+
+
+}
