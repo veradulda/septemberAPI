b87439c4a1072b5afc29293b7a4921d4fb52092d
veraDulda <99422897+veraDulda@users.noreply.github.com>
2022-09-15 16:00:41 +0300

    connection is succesful

--- src/test/java/com/khamam/firstControl.java
+++ src/test/java/com/khamam/firstControl.java
@@ -1,6 +1,22 @@
 package com.khamam;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
 public class firstControl {
 
-    
+    @Test
+    public void test1(){
+
+        String baseURL = "http://54.91.166.114:8000/";
+
+        Response response = RestAssured.get(baseURL + "api/hello");
+
+        System.out.println("response.statusCode() = " + response.statusCode());
+        response.prettyPrint();
+
+    }
+
+
 }
